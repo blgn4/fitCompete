dee1004619017477882d9a2dd6c751c38b3a8db0
Ubuntu <ubuntu@ip-172-31-1-133.us-west-2.compute.internal>
2016-06-08 08:56:49 +0000

    producer consumer minor corrections

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,7 +1,7 @@
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.subscribe(['msgpackfoo'])
+consumer.subscribe(['influxdb'])
 
 for msg in consumer:
 	print msg
--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -14,25 +14,25 @@ def generate_data():
 	start_time=int(round(time.time()*1000))
 	diff=0
 
-	tot_time=randrange(900000,1800000)	
+	tot_time=random.randrange(900000,1800000,1)	
 
 	us1=10000001
 	us2=10000002
 
-	hr1=randrange(60,200)
-	hr2=randrange(60,200)
+	hr1=random.randrange(60,200,1)
+	hr2=random.randrange(60,200,1)
 
 	while diff < tot_time:
 		d1={}
 		time_stamp=int(round(time.time()*1000))
-		d1[timestamp]=time_stamp
-		d1[heart_rate]=hr1
+		d1['timestamp']=time_stamp
+		d1['heart_rate']=hr1
 		json_data1=json.dumps(d1)
 		send_data('influxdb',json_data1)
 
 		d2={}
-		d2[timestamp]=time_stamp
-		d2[heart_rate]=hr2
+		d2['timestamp']=time_stamp
+		d2['heart_rate']=hr2
 		json_data2=json.dumps(d2)
 		send_data('influxdb',json_data2)
 
