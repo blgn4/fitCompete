afd9b90831b748e2332f24768f9723438a92e0dc
blgn4 <blgniharika@hotmail.com>
2016-06-08 01:38:53 -0700

    simple kafka producer and consumer

---
+++ kafka/kafka_producer.py
@@ -0,0 +1,47 @@
+from kafka import KafkaProducer
+import json
+import random
+import time
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+
+def send_data(topic,data):
+	producer.send(topic,data)
+
+def generate_data():
+
+	start_time=int(round(time.time()*1000))
+	diff=0
+
+	tot_time=randrange(900000,1800000)	
+
+	us1=10000001
+	us2=10000002
+
+	hr1=randrange(60,200)
+	hr2=randrange(60,200)
+
+	while diff < tot_time:
+		d1={}
+		time_stamp=int(round(time.time()*1000))
+		d1[timestamp]=time_stamp
+		d1[heart_rate]=hr1
+		json_data1=json.dumps(d1)
+		send_data('influxdb',json_data1)
+
+		d2={}
+		d2[timestamp]=time_stamp
+		d2[heart_rate]=hr2
+		json_data2=json.dumps(d2)
+		send_data('influxdb',json_data2)
+
+generate_data()
+
+
+
+
+
+
+
+
