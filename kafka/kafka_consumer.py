b082cb85dc004bd617bc5ebcdea59063c5b17bd9
blgn4 <blgniharika@hotmail.com>
2016-06-09 14:21:30 -0700

    Producer and consumer

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -9,6 +9,7 @@ consumer.subscribe(['influxdb2'])
 for msg in consumer:
 	data=msg.value
 	d=data.split(",")
+	'''
 	fields={}
 	fields['speed']=int(d[7])
 	fields['date']=d[10][:-1]
@@ -23,15 +24,15 @@ for msg in consumer:
 	tags['steps']=int(d[3])
 	tags['floors']=int(d[4])
 	tags['calories']=int(d[5])
-	'''
+	
 	data['fields']=fields
 	data['tags']=tags
 	data['measurement']='fitbit_data' '''
 	data1=[("measurement","fitbit_data") , ("tags",tags),("fields",fields)]
 	print(json.dumps(OrderedDict(data1)))
 
-	'''data='[{"measurement":"fitbit_data" , "tags":{"user_id":'+int(d[0][1:])+',"bmi":'+int(d[1])+',"fat":'+int(d[2])+',"steps":'+int(d[3])+',"floors":'+int(d[4])+',"calories":'+int(d[5])+'},"fields":{"speed":'+int(d[7])+',"calories_rate":'+int(d[6])+',"heart_rate":'+int(d[6])+',"total_time":'+int(d[9])+'"date":'+d[10][:-1] +'}}]'
-	'''
+	data1='[{"measurement":"fitbit_data" , "tags":{"user_id":'+int(d[0][1:])+',"bmi":'+int(d[1])+',"fat":'+int(d[2])+',"steps":'+int(d[3])+',"floors":'+int(d[4])+',"calories":'+int(d[5])+'},"fields":{"speed":'+int(d[7])+',"calories_rate":'+int(d[6])+',"heart_rate":'+int(d[6])+',"total_time":'+int(d[9])+'"date":'+d[10][:-1] +'}}]'
+	
 	client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex') 
 	client.write_points(data1)
 	
