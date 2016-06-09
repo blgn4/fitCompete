0d9d2aa4da39619af333616c947d38033236db09
blgn4 <blgniharika@hotmail.com>
2016-06-09 10:21:57 -0700

    included consumer for influxdb

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,7 +1,27 @@
 from kafka import KafkaConsumer
+from influxdb import InfluxDBClient
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
 consumer.subscribe(['influxdb'])
 
 for msg in consumer:
-	print msg
+	user_id=msg['user_id']
+	bmi=msg['bmi']
+	fat=msg['fat']
+	steps=msg['steps']
+	floors=msg['floors']
+	calories=msg['calories']
+	calories_rate=msg['calories_rate']
+	speed=msg['speed']
+	heart_rate=msg['heart_rate']
+	tot_time=msg['total_workout_time']
+	date=msg['date']
+
+
+json_data={"measurement":"fitbit_data","tags":{"user_id":user_id,"bmi":bmi,"fat":fat,"steps":steps,"floors":floors,"calories":calories},"fields":{"speed":speed,"date"=date,"calories_rate":calories_rate,"heart_rate":heart_rate,"total_time":tot_time}}
+
+client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex')
+client.write_points(json_data)
+
+
+
--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -14,27 +14,72 @@ def generate_data():
 	start_time=int(round(time.time()*1000))
 	diff=0
 
+	date=time.strftime("%d-%m-%Y")
+
 	tot_time=random.randrange(900000,1800000,1)	
 
 	us1=10000001
 	us2=10000002
 
-	hr1=random.randrange(60,200,1)
-	hr2=random.randrange(60,200,1)
+	
+	bmi1=random.randrange(18,35)
+	bmi2=random.randrange(18,35)
+
+	fat_range1=random.randrange(15,25)
+	fat_range2=random.ramdrange(15,25)
+
+	steps1=random.randrange(1000,10000)
+	steps2=random.randrange(1000,10000)
+
+	floors1=random.randrange(0,25)
+	floors2=random.randrange(0,25)
+
+	cal_in1=random.randrange(1500,3000)
+	cal_in2=random.randrange(1500,3000)
+
+	
 
 	while diff < tot_time:
+		hr1=random.randrange(60,200,1)
+		hr2=random.randrange(60,200,1)
+
+		speed1=random.randrange(2,6)
+		speed2=random.randrange(2,6)
+
+		cal_out_rate1=random.randrange(6,10)
+		cal_out_rate2=random.randrange(6,10)
+
 		d1={}
-		time_stamp=int(round(time.time()*1000))
-		d1['timestamp']=time_stamp
+		d1['user_id']=us1
+		d1['bmi']=bmi1
+		d1['fat']=fat_range1
+		d1['steps']=steps1
+		d1['floors']=floors1
+		d1['calories']=cal_in1
+		d1['calories_rate']=cal_out_rate1
+		d1['speed']=speed1
 		d1['heart_rate']=hr1
+		d1['total_workout_time']=tot_time
+		d1['date']=date
 		json_data1=json.dumps(d1)
 		send_data('influxdb',json_data1)
 
 		d2={}
-		d2['timestamp']=time_stamp
+		d2['user_id']=us2
+		d2['bmi']=bmi2
+		d2['fat']=fat_range2
+		d2['steps']=steps2
+		d2['floors']=floors2
+		d2['calories']=cal_in2
+		d2['calories_rate']=cal_out_rate2
+		d2['speed']=speed2
 		d2['heart_rate']=hr2
+		d2['total_workout_time']=tot_time
+		d2['date']=date
 		json_data2=json.dumps(d2)
 		send_data('influxdb',json_data2)
+
+		diff=int(round(time.time()*1000))-start_time
 
 generate_data()
 
