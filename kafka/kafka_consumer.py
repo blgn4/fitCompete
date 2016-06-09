c8acc861d09877ee149261aafe5029b2bc195308
blgn4 <blgniharika@hotmail.com>
2016-06-09 14:16:37 -0700

    minor changes

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -31,9 +31,9 @@ for msg in consumer:
 	print(json.dumps(OrderedDict(data1)))
 
 	'''data='[{"measurement":"fitbit_data" , "tags":{"user_id":'+int(d[0][1:])+',"bmi":'+int(d[1])+',"fat":'+int(d[2])+',"steps":'+int(d[3])+',"floors":'+int(d[4])+',"calories":'+int(d[5])+'},"fields":{"speed":'+int(d[7])+',"calories_rate":'+int(d[6])+',"heart_rate":'+int(d[6])+',"total_time":'+int(d[9])+'"date":'+d[10][:-1] +'}}]'
-	
-	client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex') 
-	client.write_points(data)
 	'''
+	client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex') 
+	client.write_points(data1)
+	
 
 
