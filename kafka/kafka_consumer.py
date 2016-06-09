f4ed648d88f62ceef8ade506b120103c08ab96d6
blgn4 <blgniharika@hotmail.com>
2016-06-09 13:28:29 -0700

    small change

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -8,28 +8,25 @@ consumer.subscribe(['influxdb2'])
 for msg in consumer:
 	data=msg.value
 	d=data.split(",")
+	data={}
+	data['measurement']='fitbit_data'
+	tags={}
+	tags['user_id']=d[0]
+	tags['bmi']=d[1]
+	tags['fat']=d[2]
+	tags['steps']=d[3]
+	tags['floors']=d[4]
+	tags['calories']=d[5]
+	data['tags']=tags
+	fields={}
+	fields['speed']=d[7]
+	fields['date']=date[9]
+	fields['calories_rate']=d[6]
+	fields['heart_rate']=d[8]
+	fields['total_time']=d[10]
+	data['fields']=fields
 
-
-
-data={}
-data['measurement']='fitbit_data'
-tags={}
-tags['user_id']=d[0]
-tags['bmi']=d[1]
-tags['fat']=d[2]
-tags['steps']=d[3]
-tags['floors']=d[4]
-tags['calories']=d[5]
-data['tags']=tags
-fields={}
-fields['speed']=d[7]
-fields['date']=date[9]
-fields['calories_rate']=d[6]
-fields['heart_rate']=d[8]
-fields['total_time']=d[10]
-data['fields']=fields
-
-print(data)
+	print(data)
 
 '''
 client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex')
