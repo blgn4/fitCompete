e58301195714abed8ad1fcb590e79e9d1f803513
blgn4 <blgniharika@hotmail.com>
2016-06-09 12:57:36 -0700

    small changes

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -6,32 +6,35 @@ consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
 consumer.subscribe(['influxdb2'])
 
 for msg in consumer:
-	print msg
+	data=msg.value
+	d=data.split(",")
 
 
-'''
+
 data={}
 data['measurement']='fitbit_data'
 tags={}
-tags['user_id']=user_id
-tags['bmi']=bmi
-tags['fat']=fat
-tags['steps']=steps
-tags['floors']=floors
-tags['calories']=calories
+tags['user_id']=d[0]
+tags['bmi']=d[1]
+tags['fat']=d[2]
+tags['steps']=d[3]
+tags['floors']=d[4]
+tags['calories']=d[5]
 json_tags=json.dumps(tags)
 data['tags']=json_tags
 fields={}
-fields['speed']=speed
-fields['date']=date
-fields['calories_rate']=calories_rate
-fields['heart_rate']=heart_rate
-fields['total_time']=tot_time
+fields['speed']=d[7]
+fields['date']=date[9]
+fields['calories_rate']=d[6]
+fields['heart_rate']=d[8]
+fields['total_time']=d[10]
 json_fields=json.dump(fields)
 data['fields']=json_fields
 json_data=json.dump(data)
 
+print json_data
 
+'''
 client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex')
 client.write_points(json_data)
 '''
