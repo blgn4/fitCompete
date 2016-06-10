f19fd879a7a323071157e8aae958d6c828c41527
blgn4 <blgniharika@hotmail.com>
2016-06-10 02:53:49 -0700

    changes made

--- FLask/app/views.py
+++ FLask/app/views.py
@@ -1,8 +1,12 @@
 from app import app
+import redis
+
+rediscon=redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 
 @app.route('/index')
 def index():
-	return "Hello World!!"
+	elements=redis.lrange('competetors10-06-2016',0,-1)
+	return elements
 
