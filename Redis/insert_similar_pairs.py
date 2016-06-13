1de8552410ec76d10007f97e72f43211679204fa
blgn4 <blgniharika@hotmail.com>
2016-06-12 23:25:17 -0700

    redis initial data insertion

--- Redis/insert_similar_pairs.py
+++ Redis/insert_similar_pairs.py
@@ -2,11 +2,12 @@ import redis
 import time
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-today_date="competetors"+time.strftime("%d-%m-%Y")
-uid=100000
-for i in range(1,100):
+today_date="competetors"
+uid=1000000
+for i in range(1,100000):
 	u1=uid+i
-	u2=2*uid+i
+	i+=1
+	u2=uid+i
 	print(str(u1))
 	comp=str(u1)+","+str(u2)
 	r.lpush(today_date,comp)
