36aa1486262c1606ba63507b4772da8c435e416e
blgn4 <blgniharika@hotmail.com>
2016-06-13 14:45:25 -0700

    changes

--- kafka/kafka_to_influxdb.py
+++ kafka/kafka_to_influxdb.py
@@ -3,14 +3,15 @@ from kafka import KafkaConsumer
 from influxdb import InfluxDBClient
 from collections import OrderedDict
 
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.subscribe('influx5')
+consumer = KafkaConsumer('influx5',bootstrap_servers='localhost:9092')
+
 client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha') 
 
 for msg in consumer:
+	print msg
 	data=msg.value
 	d=data.split(",")
-	print msg
+	
 	data1=[{"measurement":"fitbit_data" , "tags":{"user_id":int(d[0][1:]),"bmi":+int(d[1]),"fat":int(d[2]),"steps":int(d[3]),"floors":int(d[4]),"calories":int(d[5])},"fields":{"speed":int(d[7]),"calories_rate":int(d[6]),"heart_rate":int(d[6]),"total_time":int(d[9]),"date":d[10][:-1] }}]
 	
 	client.write_points(data1)
