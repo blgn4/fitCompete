6c1cb280d98f0d880bc55413a92d810d7ddd0ba4
blgn4 <blgniharika@hotmail.com>
2016-06-13 14:23:20 -0700

    changes

--- kafka/kafka_to_influxdb.py
+++ kafka/kafka_to_influxdb.py
@@ -10,7 +10,7 @@ client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'
 for msg in consumer:
 	data=msg.value
 	d=data.split(",")
-
+	print msg
 	data1=[{"measurement":"fitbit_data" , "tags":{"user_id":int(d[0][1:]),"bmi":+int(d[1]),"fat":int(d[2]),"steps":int(d[3]),"floors":int(d[4]),"calories":int(d[5])},"fields":{"speed":int(d[7]),"calories_rate":int(d[6]),"heart_rate":int(d[6]),"total_time":int(d[9]),"date":d[10][:-1] }}]
 	
 	client.write_points(data1)
