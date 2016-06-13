83669ae6de21f412d16299c58dada67f8dc6fbe0
blgn4 <blgniharika@hotmail.com>
2016-06-13 10:48:25 -0700

    Added consumer for influxdb

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -4,36 +4,15 @@ from influxdb import InfluxDBClient
 from collections import OrderedDict
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-consumer.subscribe(['influxdb2'])
+consumer.subscribe(['influx'])
 
 for msg in consumer:
 	data=msg.value
 	d=data.split(",")
-	'''
-	fields={}
-	fields['speed']=int(d[7])
-	fields['date']=d[10][:-1]
-	fields['calories_rate']=int(d[6])
-	fields['heart_rate']=int(d[8])
-	fields['total_time']=int(d[9])
-	
-	tags={}
-	tags['user_id']=int(d[0][1:])
-	tags['bmi']=int(d[1])
-	tags['fat']=int(d[2])
-	tags['steps']=int(d[3])
-	tags['floors']=int(d[4])
-	tags['calories']=int(d[5])
-	
-	data['fields']=fields
-	data['tags']=tags
-	data['measurement']='fitbit_data' 
-	data1=[("measurement","fitbit_data") , ("tags",tags),("fields",fields)]
-	print(json.dumps(OrderedDict(data1)))'''
 
 	data1=[{"measurement":"fitbit_data" , "tags":{"user_id":int(d[0][1:]),"bmi":+int(d[1]),"fat":int(d[2]),"steps":int(d[3]),"floors":int(d[4]),"calories":int(d[5])},"fields":{"speed":int(d[7]),"calories_rate":int(d[6]),"heart_rate":int(d[6]),"total_time":int(d[9]),"date":d[10][:-1] }}]
 	
-	client=InfluxDBClient('localhost',8086,'root','root','niha_db_ex') 
+	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha') 
 	client.write_points(data1)
 	
 
