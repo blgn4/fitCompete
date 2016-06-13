6fe35eb6bcd6d26ae6dea4c6f78e0ef599469cfe
blgn4 <blgniharika@hotmail.com>
2016-06-13 01:46:36 -0700

    Added threading for producer

---
+++ kafka/read_from_redis.py
@@ -0,0 +1,85 @@
+import redis
+import thread
+import time
+from kafka import KafkaProducer
+import random
+import threading
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+
+class userThreads (threading.Thread):
+    def __init__(self,counter, user_1,user_2):
+        threading.Thread.__init__(self)
+        self.threadID = counter
+        self.name = str(user_1)+str(user_2)
+    def run(self):
+        print "Starting " + self.name
+        generate_data(user_1, user_2)
+        print "Exiting " + self.name
+
+def obtain_users_from_redis():
+	r = redis.StrictRedis(host='localhost', port=6379, db=0)
+	key="competetors"
+	count=1
+	while r.llen(key)!=0:
+		user_pair=r.lpop(key)
+		user=user_pair.split(',')
+		sleep(1)
+		thread=userThreads(count,user[0],user[1])
+		count=count+1
+		
+		
+
+def generate_data(us1,us2):
+
+	start_time=int(round(time.time()*1000))
+	diff=0
+
+	date=time.strftime("%d-%m-%Y")
+
+	tot_time=random.randrange(900000,1800000,1)	
+
+	
+	bmi1=random.randrange(18,35)
+	bmi2=random.randrange(18,35)
+
+	fat_range1=random.randrange(15,25)
+	fat_range2=random.randrange(15,25)
+
+	steps1=random.randrange(1000,10000)
+	steps2=random.randrange(1000,10000)
+
+	floors1=random.randrange(0,25)
+	floors2=random.randrange(0,25)
+
+	cal_in1=random.randrange(1500,3000)
+	cal_in2=random.randrange(1500,3000)
+
+	
+
+	while diff < tot_time:
+		sleep(1)
+		hr1=random.randrange(60,200,1)
+		hr2=random.randrange(60,200,1)
+
+		speed1=random.randrange(2,6)
+		speed2=random.randrange(2,6)
+
+		cal_out_rate1=random.randrange(6,10)
+		cal_out_rate2=random.randrange(6,10)
+
+		data1=str(us1)+","+str(bmi1)+","+str(fat_range1)+","+str(steps1)+","+str(floors1)+","+str(cal_in1)+","+str(cal_out_rate1)+","+str(speed1)+","+str(hr1)+","+str(tot_time)+","+date
+		send_data('influxdb2',data1)
+
+		data2=str(us2)+","+str(bmi2)+","+str(fat_range2)+","+str(steps2)+","+str(floors2)+","+str(cal_in2)+","+str(cal_out_rate2)+","+str(speed2)+","+str(hr2)+","+str(tot_time)+","+date
+		send_data('influxdb2',data2)
+
+		diff=int(round(time.time()*1000))-start_time
+
+def send_data(topic,data):
+	producer.send(topic,data)
+
+
+
+
