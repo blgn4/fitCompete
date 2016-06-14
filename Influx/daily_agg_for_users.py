42cc65cb8840fed0e0e72feff999062a9920df10
blgn4 <blgniharika@hotmail.com>
2016-06-14 10:50:57 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -12,7 +12,12 @@ def generate_user_profiles():
 		que = "select mean(speed),mean(calories_rate),mean(heart_rate) from fitbit_data where user_id='"+str(key)+"' and date='"+value +"' group by user_id,bmi,fat,steps,floors,calories,total_time,date"
 		res = client.query(que)
 		print 'This is the first result set element:\n'+res
-		for key,value in res[0].iteritems():
+		for x in res:
+			tags=x[0].keys()[0]
+			values=x[0]
+			print tags
+			print values
+			'''
 			user_id=key[1]['user_id']
 			total_time=key[1]['total_time']
 			bmi=key[1]['bmi']
@@ -25,7 +30,7 @@ def generate_user_profiles():
 			heart_rate=val[0]['mean_2']
 			data2={"measurement":"final_data1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}
 			client.write_point(data2)
-
+			'''
 
 
 generate_user_profiles()
