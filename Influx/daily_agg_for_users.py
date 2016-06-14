d9e7f7b37ea8c853e065e51abc24172ee6631f4c
blgn4 <blgniharika@hotmail.com>
2016-06-13 17:59:45 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -1,10 +1,13 @@
 from influxdb import InfluxDBClient
 
 def generate_user_profiles():
+	user_date={}
 	client=InfluxDBClient('localhost',8086,'root','root','niha')
 	query='select user_id,last(date) from fitbit_data group by  user_id'
 	result = client.query(query)
 	for x in result:
-		print x
+		print x[1][user_id]
+
+
 
 generate_user_profiles()
