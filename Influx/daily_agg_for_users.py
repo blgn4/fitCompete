2580bd701d8f560201dab319852a1f2ebec12e20
blgn4 <blgniharika@hotmail.com>
2016-06-13 18:04:29 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -6,7 +6,8 @@ def generate_user_profiles():
 	query='select user_id,last(date) from fitbit_data group by  user_id'
 	result = client.query(query)
 	for x in result:
-		print x[0]['user_id']
+		user_date[x[0]['user_id']] = x[0]['date']
+	print user_date
 
 
 
