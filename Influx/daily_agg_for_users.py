d461921bcd4a6b36b834201a4727a257c0480191
blgn4 <blgniharika@hotmail.com>
2016-06-14 00:02:09 -0700

    added influx aggregation queries

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -9,6 +9,7 @@ def generate_user_profiles():
 		user_date[x[0]['user_id']] = x[0]['last']
 	for key, value in user_date.iteritems():
 		que = 'select mean(speed),mean(calories_rate),mean(heart_rate) from fitbit_data where user_id='+str(key)+'and date='+value +' group by user_id,bmi,fat,steps,floors,calories,total_time,date'
+		print que
 		res = client.query(que)
 		print res
 
