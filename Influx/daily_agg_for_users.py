3e7d0fe9c3368971133ca1aa132b52964ed6f57a
blgn4 <blgniharika@hotmail.com>
2016-06-14 13:01:53 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -11,26 +11,24 @@ def generate_user_profiles():
 		que = "select mean(speed),mean(calories_rate),mean(heart_rate) from fitbit_data where user_id='"+str(key)+"' and date='"+value +"' group by user_id,bmi,fat,steps,floors,calories,total_time,date"
 		res = client.query(que)
 		res1= res.raw
-		vals=res1['series']
-		print vals
-		tags=res1['tags']
-
+		series=res1['series'][0]
+		vals=series['values'][0]
+		tags=series['tags']
+		user_id=tags['user_id']
+		total_time=tags['total_time']
+		bmi=tags['bmi']
+		fat=tags['fat']
+		steps=tags['steps']
+		floors=tags['floors']
+		calories=tags['calories']
+		speed=vals[1]
+		calories_rate=val[1]
+		heart_rate=val[2]
+		data2={"measurement":"final_data1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}
+		print data2
 		
-
-		print '\n'+tags
 		
 		'''
-		user_id=key[1]['user_id']
-		total_time=key[1]['total_time']
-		bmi=key[1]['bmi']
-		fat=key[1]['fat']
-		steps=key[1]['steps']
-		floors=key[1]['floors']
-		calories=key[1]['calories']
-		speed=val[0]['mean']
-		calories_rate=val[0]['mean_1']
-		heart_rate=val[0]['mean_2']
-		data2={"measurement":"final_data1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}
 		client.write_point(data2)
 		'''
 
