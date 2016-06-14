bcf5f339f2c75566d906a53d776401965e479164
blgn4 <blgniharika@hotmail.com>
2016-06-13 17:46:06 -0700

    added influx query1

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -1,3 +1,5 @@
+from influxdb import InfluxDBClient
+
 def generate_user_profiles():
 	client=InfluxDBClient('localhost',8086,'root','root','niha')
 	query='select user_id,last(date) from fitbit_data group by  user_id'
