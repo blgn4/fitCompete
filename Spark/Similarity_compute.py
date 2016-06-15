1e6bd46679abeb3b77969c9180d06ab398dd6016
blgn4 <blgniharika@hotmail.com>
2016-06-14 17:46:16 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -3,7 +3,7 @@ from influxdb import InfluxDBClient
 
 def get_data_from_influx():
 	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
-	query='select * from fitbit_data1'
+	query='select * from final_data1'
 	result=client.query(query)
 	print result
 
