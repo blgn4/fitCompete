c6bfdca82d29167bde5bd22a20aa8e4974ba393f
blgn4 <blgniharika@hotmail.com>
2016-06-14 17:50:27 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -5,7 +5,7 @@ def get_data_from_influx():
 	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
 	query='select * from final_data1'
 	result=client.query(query)
-	print result
+	print result[0]
 
 get_data_from_influx()
 
