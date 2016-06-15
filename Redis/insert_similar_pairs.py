0b9ef107d6387f3026209c5aa53c988492d5971a
blgn4 <blgniharika@hotmail.com>
2016-06-15 14:25:41 -0700

    redis add data into timeperiods

--- Redis/insert_similar_pairs.py
+++ Redis/insert_similar_pairs.py
@@ -2,14 +2,54 @@ import redis
 import time
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-key="competetors"
-uid=1000000
+key="timeperiod1"
+uid=100000
 i=1
-while i<=100000:
+while i<=29999:
 	u1=uid+i
 	i=i+1
 	u2=uid+i
 	i=i+1
-	print(str(u1))
 	comp=str(u1)+","+str(u2)
 	r.lpush(key,comp)
+
+print i
+key="timeperiod2"
+while i<=45001:
+	u1=uid+i
+	i=i+1
+	u2=uid+i
+	i=i+1
+	comp=str(u1)+","+str(u2)
+	r.lpush(key,comp)
+print i
+key="timeperiod3"
+while i<=75001:
+	u1=uid+i
+	i=i+1
+	u2=uid+i
+	i=i+1
+	comp=str(u1)+","+str(u2)
+	r.lpush(key,comp)
+print i
+key="timeperiod4"
+while i<=90001:
+	u1=uid+i
+	i=i+1
+	u2=uid+i
+	i=i+1
+	comp=str(u1)+","+str(u2)
+	r.lpush(key,comp)
+print i
+key="timeperiod5"
+while i<=99999:
+	u1=uid+i
+	i=i+1
+	u2=uid+i
+	i=i+1
+	comp=str(u1)+","+str(u2)
+	r.lpush(key,comp)
+print i
+
+
+
