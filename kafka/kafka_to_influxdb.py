d6fad746fb2264213b421c04f8fd36ddd83ccf78
blgn4 <blgniharika@hotmail.com>
2016-06-16 13:06:46 -0700

    changes

--- kafka/kafka_to_influxdb.py
+++ kafka/kafka_to_influxdb.py
@@ -1,6 +1,5 @@
 from kafka import KafkaConsumer
 from influxdb import InfluxDBClient
-from collections import OrderedDict
 
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
 consumer.subscribe(['week3_topic'])
@@ -8,9 +7,9 @@ consumer.subscribe(['week3_topic'])
 
 for msg in consumer:
         data=msg.value
+        print data
         d=data.split(",")
         data1=[{"measurement":"week3_try1","tags":{"user_id":int(d[0][1:])},"fields":{"heart_rate":int(d[1]),"speed":int(d[2]),"calories_rate":int(d[3]),"date":d[4][:-1]}}]
-        print d[0][1:]
         client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
         client.write_points(data1)
 
