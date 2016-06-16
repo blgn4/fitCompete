8aab4fbd2fc8cfcd8308ce39f77841e6c0cae15b
blgn4 <blgniharika@hotmail.com>
2016-06-15 23:11:12 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -85,7 +85,8 @@ def split_string(s):
 def write_into_redis(s):
 	redis_client = redis.StrictRedis(host='ec2-52-10-235-49.us-west-2.compute.amazonaws.com', port=6379, db=0)
 	pipe = redis_client.pipeline()
-	pipe.lpush('ex_groups',s)
+	for i in s:
+		pipe.lpush('ex_groups',i)
 	pipe.execute()
 
 
