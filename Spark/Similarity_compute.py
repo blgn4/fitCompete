1c86aac149bde54c2e6fa9fd0ff78f7caa743a1e
blgn4 <blgniharika@hotmail.com>
2016-06-15 23:19:47 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -86,8 +86,7 @@ def write_into_redis(s):
 	redis_client = redis.StrictRedis(host='ec2-52-10-235-49.us-west-2.compute.amazonaws.com', port=6379, db=0)
 	pipe = redis_client.pipeline()
 	for i in s:
-		print i[1]
-		pipe.lpush('ex_groups',i)
+		pipe.lpush('ex_groups',i[1])
 	pipe.execute()
 
 
