c8a3f04eb053d9354130ac3541fd73bb4f4b1e46
blgn4 <blgniharika@hotmail.com>
2016-06-17 06:50:25 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -2,9 +2,8 @@ from pyspark import SparkContext, SparkConf
 from influxdb import InfluxDBClient
 import redis
 
-#redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0,password='')
-#redis_client.flushall()
-
+redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0,password='')
+redis.flushall()
 
 def get_data_from_influx():
 	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
@@ -13,7 +12,6 @@ def get_data_from_influx():
 	res=result.raw
 	series=res['series'][0]
 	vals=series['values']
-	print res
 	str2=[]
 	for val in vals:
 		str1=''		
@@ -24,67 +22,57 @@ def get_data_from_influx():
 			str1+='M'
 		else:
 			str1+='H'
-
-
-		fat=val[2]
-		if fat < 18:
-			str1+='L'
-		elif fat >= 18 and calories_rate <=21:
-			str1+='M'
-		else:
-			str1+='H'
-
-		steps=val[3]
-		if steps <3000:
-			str1+='L'
-		elif steps >= 3000 and steps <= 6000:
-			str1+='M'
-		else:
-			str1+='H'
-
-
-		floors=val[4]
-		if floors < 8:
-			str1+='L'
-		elif floors >=8 and floors <= 16:
-			str1+='M'
-		else:
-			str1+='H'
 		
-		calories=val[5]
+		calories=val[2]
 		if calories < 2000:
 			str1+='L'
 		elif calories >= 2000 and calories <=2500:
 			str1+='M'
 		else:
 			str1+='H'
-
-		speed=val[6]
-		if speed < 4:
-			str1+='L'
-		elif speed >=4 and speed <5:
-			str1+='M'
-		else:
-			str1+='H'
 		
-		calories_rate=val[7]
+		calories_rate=val[3]
 		if calories_rate < 8:
 			str1+='L'
 		elif calories_rate >= 8 and calories_rate <=9:
 			str1+='M'
 		else:
 			str1+='H'
-		
-		
-		heart_rate=val[8]
+		fat=val[4]
+		if fat < 18:
+			str1+='L'
+		elif fat >= 18 and calories_rate <=21:
+			str1+='M'
+		else:
+			str1+='H'
+		floors=val[5]
+		if floors < 8:
+			str1+='L'
+		elif floors >=8 and floors <= 16:
+			str1+='M'
+		else:
+			str1+='H'
+		heart_rate=val[6]
 		if heart_rate < 107:
 			str1+='L'
 		elif heart_rate>=107 and heart_rate <= 153:
 			str1+='M'
 		else:
 			str1+='H'
-		
-		
+		speed=val[7]
+		if speed < 4:
+			str1+='L'
+		elif speed >=4 and speed <5:
+			str1+='M'
+		else:
+			str1+='H'
+		steps=val[8]
+		if steps <3000:
+			str1+='L'
+		elif steps >= 3000 and steps <= 6000:
+			str1+='M'
+		else:
+			str1+='H'
 		user_id=val[9]
 		str1+=','
 		str1+=user_id
@@ -96,7 +84,7 @@ def split_string(s):
 	return (tup[0],[tup[1]])
 
 def write_into_redis(s):
-	redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0,password='')
+	redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0)
 	pipe = redis_client.pipeline()
 	for i in s:
 		pipe.lpush(i[0],*i[1])
@@ -105,19 +93,19 @@ def write_into_redis(s):
 
 
 
-# appName='Similarity_APP'
-# master='spark://ec2-52-40-200-26.us-west-2.compute.amazonaws.com:7077'
-# conf = SparkConf().setAppName(appName).setMaster(master)
-# sc = SparkContext(conf=conf)
+appName='Similarity_APP'
+master='spark://ec2-52-40-200-26.us-west-2.compute.amazonaws.com:7077'
+conf = SparkConf().setAppName(appName).setMaster(master)
+sc = SparkContext(conf=conf)
 
 list_1 = get_data_from_influx()
-# rdd = sc.parallelize(list_1)
+rdd = sc.parallelize(list_1)
 
-# tupls=rdd.map(split_string)
+tupls=rdd.map(split_string)
 
-# buckets=tupls.reduceByKey(lambda a,b: a+b)
-# write_into_redis.count=0
-# buckets.foreachPartition(write_into_redis)
+buckets=tupls.reduceByKey(lambda a,b: a+b)
+write_into_redis.count=0
+buckets.foreachPartition(write_into_redis)
 
 
 
