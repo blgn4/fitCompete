64eacee3ab3cc511704dc99ec78ba03c098b3ebb
blgn4 <blgniharika@hotmail.com>
2016-06-17 10:21:57 -0700

    changes

--- FLask/app/views.py
+++ FLask/app/views.py
@@ -1,13 +1,78 @@
 from app import app
 import redis
+import jsonify
 
-rediscon=redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+
+def redis_counts(key_pattern):
+	leng=0
+	rediscon=redis.StrictRedis(host='localhost', port=6379, db=0,password='')
+	keys=rediscon.keys(pattern=key_pattern)
+	for key in keys:
+		leng+=rediscon.llen(key)
+	return leng
+
 
 @app.route('/')
 
 @app.route('/index')
 def index():
 	return 'Fit Compete Analytics'
+
+@app.route('/<feature>')
+def get_counts_for_feature(feature):
+	if feature=='bmi':
+		low=redis_counts('L???????')
+		medium=redis_counts('M???????')
+		high=redis_counts('H???????')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+
+	elif feature=='calories':
+		low=redis_counts('?L??????')
+		medium=redis_counts('?M??????')
+		high=redis_counts('?H??????')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+		
+	elif feature=='calories_rate':
+		low=redis_counts('??L?????')
+		medium=redis_counts('??M?????')
+		high=redis_counts('??H?????')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+	elif feature=='fat':
+		low=redis_counts('???L????')
+		medium=redis_counts('???M????')
+		high=redis_counts('???H????')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+	elif feature=='floors':
+		low=redis_counts('????L???')
+		medium=redis_counts('????M???')
+		high=redis_counts('????H???')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+	elif feature=='heart_rate':
+		low=redis_counts('?????L??')
+		medium=redis_counts('?????M??')
+		high=redis_counts('?????H??')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+	elif feature=='speed':
+		low=redis_counts('??????L?')
+		medium=redis_counts('??????M?')
+		high=redis_counts('??????H?')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+	elif feature=='steps':
+		low=redis_counts('???????L')
+		medium=redis_counts('???????M')
+		high=redis_counts('???????H')
+		ress=[{"low":low,"medium":medium,"high":high}]
+		return jsonify(result=ress)
+
 
 
 
