8fc73ea0753d7eb5e553e2d29c3ace8ec43b6e90
blgn4 <blgniharika@hotmail.com>
2016-06-17 06:39:37 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -5,6 +5,8 @@ import redis
 redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0,password='')
 redis_client.flushall()
 
+"heart_rate":int(float(heart_rate))}
+
 def get_data_from_influx():
 	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
 	query='select * from week3_final1'
@@ -12,6 +14,7 @@ def get_data_from_influx():
 	res=result.raw
 	series=res['series'][0]
 	vals=series['values']
+	print vals
 	str2=[]
 	for val in vals:
 		str1=''		
@@ -22,57 +25,67 @@ def get_data_from_influx():
 			str1+='M'
 		else:
 			str1+='H'
-		
-		calories=val[2]
-		if calories < 2000:
-			str1+='L'
-		elif calories >= 2000 and calories <=2500:
-			str1+='M'
-		else:
-			str1+='H'
-		
-		calories_rate=val[3]
-		if calories_rate < 8:
-			str1+='L'
-		elif calories_rate >= 8 and calories_rate <=9:
-			str1+='M'
-		else:
-			str1+='H'
-		fat=val[4]
+
+
+		fat=val[2]
 		if fat < 18:
 			str1+='L'
 		elif fat >= 18 and calories_rate <=21:
 			str1+='M'
 		else:
 			str1+='H'
-		floors=val[5]
-		if floors < 8:
-			str1+='L'
-		elif floors >=8 and floors <= 16:
-			str1+='M'
-		else:
-			str1+='H'
-		heart_rate=val[6]
-		if heart_rate < 107:
-			str1+='L'
-		elif heart_rate>=107 and heart_rate <= 153:
-			str1+='M'
-		else:
-			str1+='H'
-		speed=val[7]
-		if speed < 4:
-			str1+='L'
-		elif speed >=4 and speed <5:
-			str1+='M'
-		else:
-			str1+='H'
-		steps=val[8]
+
+		steps=val[3]
 		if steps <3000:
 			str1+='L'
 		elif steps >= 3000 and steps <= 6000:
 			str1+='M'
 		else:
 			str1+='H'
+
+
+		floors=val[4]
+		if floors < 8:
+			str1+='L'
+		elif floors >=8 and floors <= 16:
+			str1+='M'
+		else:
+			str1+='H'
+		
+		calories=val[5]
+		if calories < 2000:
+			str1+='L'
+		elif calories >= 2000 and calories <=2500:
+			str1+='M'
+		else:
+			str1+='H'
+
+		speed=val[6]
+		if speed < 4:
+			str1+='L'
+		elif speed >=4 and speed <5:
+			str1+='M'
+		else:
+			str1+='H'
+		
+		calories_rate=val[7]
+		if calories_rate < 8:
+			str1+='L'
+		elif calories_rate >= 8 and calories_rate <=9:
+			str1+='M'
+		else:
+			str1+='H'
+		
+		
+		heart_rate=val[8]
+		if heart_rate < 107:
+			str1+='L'
+		elif heart_rate>=107 and heart_rate <= 153:
+			str1+='M'
+		else:
+			str1+='H'
+		
+		
 		user_id=val[9]
 		str1+=','
 		str1+=user_id
@@ -93,19 +106,19 @@ def write_into_redis(s):
 
 
 
-appName='Similarity_APP'
-master='spark://ec2-52-40-200-26.us-west-2.compute.amazonaws.com:7077'
-conf = SparkConf().setAppName(appName).setMaster(master)
-sc = SparkContext(conf=conf)
+# appName='Similarity_APP'
+# master='spark://ec2-52-40-200-26.us-west-2.compute.amazonaws.com:7077'
+# conf = SparkConf().setAppName(appName).setMaster(master)
+# sc = SparkContext(conf=conf)
 
 list_1 = get_data_from_influx()
-rdd = sc.parallelize(list_1)
+# rdd = sc.parallelize(list_1)
 
-tupls=rdd.map(split_string)
+# tupls=rdd.map(split_string)
 
-buckets=tupls.reduceByKey(lambda a,b: a+b)
-write_into_redis.count=0
-buckets.foreachPartition(write_into_redis)
+# buckets=tupls.reduceByKey(lambda a,b: a+b)
+# write_into_redis.count=0
+# buckets.foreachPartition(write_into_redis)
 
 
 
