e8924aa8192022a5c8c2705c321d1005b43b51a2
blgn4 <blgniharika@hotmail.com>
2016-06-20 13:31:15 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -14,9 +14,10 @@ client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'
 query='select user_id,last(date) from week3_try1 group by  user_id'
 
 result = client.query(query)
+res=result.raw
 start_time=time.time()
 
-user_date_raw=sc.parallelize(result)
+user_date_raw=sc.parallelize(res)
 # for x in result:
 # 	user_date[x[0]['user_id']] = x[0]['last']
 
