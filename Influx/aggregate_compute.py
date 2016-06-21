72f9c8df22fe5f19302dc0d11435ccf19e39253a
blgn4 <blgniharika@hotmail.com>
2016-06-21 10:48:56 -0700

    changes

--- Influx/aggregate_compute.py
+++ Influx/aggregate_compute.py
@@ -5,7 +5,7 @@ import time
 client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
 
 start_time=time.time()
-que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1 group by user_id"
+que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1  where date='16-06-2016' group by user_id"
 res = client.query(que)
 res1= res.raw
 print("--- %s seconds ---" % (time.time() - start_time))
