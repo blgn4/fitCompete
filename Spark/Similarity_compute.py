190351b6ae90b1174d4d3ceccfa32b5a392528e3
blgn4 <blgniharika@hotmail.com>
2016-06-21 12:58:04 -0700

    changes

--- Spark/Similarity_compute.py
+++ Spark/Similarity_compute.py
@@ -7,16 +7,11 @@ redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazona
 redis_client.flushall()
 
 def get_data_from_influx():
-	client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
-	query='select * from week3_final1'
-	result=client.query(query)
-	res=result.raw
-	series=res['series'][0]
-	vals=series['values']
-	str2=[]
-	for val in vals:
+	user_list=redis_client.keys('*user*')
+	for user in user_list:
+		val=redis_client.lrange(user,0,-1)
 		str1=''		
-		bmi=val[1]
+		bmi=val[0]
 		if bmi <24:
 			str1+='L'
 		elif bmi>=24 and bmi<30:
@@ -24,7 +19,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 		
-		calories=val[2]
+		calories=val[1]
 		if calories < 2000:
 			str1+='L'
 		elif calories >= 2000 and calories <=2500:
@@ -32,7 +27,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 		
-		calories_rate=val[3]
+		calories_rate=val[2]
 		if calories_rate < 8:
 			str1+='L'
 		elif calories_rate >= 8 and calories_rate <=9:
@@ -40,7 +35,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		fat=val[4]
+		fat=val[3]
 		if fat < 18:
 			str1+='L'
 		elif fat >= 18 and calories_rate <=21:
@@ -48,7 +43,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		floors=val[5]
+		floors=val[4]
 		if floors < 8:
 			str1+='L'
 		elif floors >=8 and floors <= 16:
@@ -56,7 +51,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		heart_rate=val[6]
+		heart_rate=val[5]
 		if heart_rate < 107:
 			str1+='L'
 		elif heart_rate>=107 and heart_rate <= 153:
@@ -64,7 +59,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		speed=val[8]
+		speed=val[6]
 		if speed < 4:
 			str1+='L'
 		elif speed >=4 and speed <5:
@@ -72,7 +67,7 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		steps=val[9]
+		steps=val[8]
 		if steps <3000:
 			str1+='L'
 		elif steps >= 3000 and steps <= 6000:
@@ -80,15 +75,15 @@ def get_data_from_influx():
 		else:
 			str1+='H'
 
-		period=val[7]
+		period=val[6]
 		if period==0:
 			str1+='M'
 		elif period==1:
 			str1+='A'
 		elif period==2:
 			str1+='E'
-			
-		user_id=val[10]
+
+		user_id=user.split(':')[1]
 		str1+=','
 		str1+=user_id
 		str2.append(str1)
@@ -99,7 +94,7 @@ def split_string(s):
 	return (tup[0],[tup[1]])
 
 def write_into_redis(s):
-	redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0)
+	redis_client = redis.StrictRedis(host='ec2-52-40-47-83.us-west-2.compute.amazonaws.com', port=6379, db=0,password='')
 	pipe = redis_client.pipeline()
 	for i in s:
 		pipe.lpush(i[0],*i[1])
