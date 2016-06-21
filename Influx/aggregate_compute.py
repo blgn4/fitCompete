1682214f678309fde7a00fff369ac815a1152830
blgn4 <blgniharika@hotmail.com>
2016-06-21 10:58:31 -0700

    changes

--- Influx/aggregate_compute.py
+++ Influx/aggregate_compute.py
@@ -8,4 +8,17 @@ start_time=time.time()
 que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1  where date='16-06-2016' group by user_id"
 res = client.query(que)
 res1= res.raw
+series=res1['series']
+for val in series:
+	values=val['values'][0]
+	tags=val['tags']
+	user_id=tags['user_id']
+	speed=vals[0]
+	calories_rate=vals[1]
+	heart_rate=vals[2]
+	bmi=random.randrange(18,35)
+	fat=random.randrange(15,25)
+	steps=random.randrange(1000,10000)
+	floors=random.randrange(0,25)
+	calories=random.randrange(1500,3000)
 print("--- %s seconds ---" % (time.time() - start_time))
