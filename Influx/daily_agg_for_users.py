ce4424e6c1cf13f20fc1f376a1249479d3e4908b
blgn4 <blgniharika@hotmail.com>
2016-06-20 22:49:17 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -18,9 +18,7 @@ def write_into_influx(s):
 	for i in s:
 		print i
 		que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1 where user_id='"+i[0]+"' and date='"+i[1]+"' group by user_id"
-		start_time= time.time()
 		res = client.query(que)
-		print("--- %s seconds ---" % (time.time() - start_time))
 		res1= res.raw
 		series=res1['series'][0]
 		vals=series['values'][0]
@@ -30,13 +28,14 @@ def write_into_influx(s):
 		calories_rate=vals[1]
 		heart_rate=vals[2]
 
-		bmi=random.randrange(18,35)
-		fat=random.randrange(15,25)
-		steps=random.randrange(1000,10000)
-		floors=random.randrange(0,25)
-		calories=random.randrange(1500,3000)
-		data2=[{"measurement":"week4_final1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}]
-		client.write_points(data2)
+		# bmi=random.randrange(18,35)
+		# fat=random.randrange(15,25)
+		# steps=random.randrange(1000,10000)
+		# floors=random.randrange(0,25)
+		# calories=random.randrange(1500,3000)
+		# data2=[{"measurement":"week4_final1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}]
+		data2=[{"measurement":"week4_final1","tags":{"user_id":user_id},"fields":{"speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}]
+		# client.write_points(data2)
 
 		
 		
