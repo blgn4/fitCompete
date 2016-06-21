c529bf76383be86d081e15c02a252813bb4008ed
blgn4 <blgniharika@hotmail.com>
2016-06-21 10:23:28 -0700

    changes

---
+++ Influx/aggregate_compute.py
@@ -0,0 +1,18 @@
+from influxdb import InfluxDBClient
+import time
+
+
+client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
+
+start_time=time.time()
+query='select user_id,last(date) from week3_try1 group by  user_id where time <= now() -1d'
+result = client.query(query)
+res=result.raw
+count =0
+series=res['series']
+for val in series:
+	i=val['values'][0]
+	que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1 where user_id='"+i[0]+"' and date='"+i[1]+"' group by user_id"
+	res = client.query(que)
+	res1= res.raw
+print("--- %s seconds ---" % (time.time() - start_time))
