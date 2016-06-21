f0aad54e0c73bb4dce356c3a9d1eedfa363076ce
blgn4 <blgniharika@hotmail.com>
2016-06-20 21:50:38 -0700

    changes

--- Influx/daily_agg_for_users.py
+++ Influx/daily_agg_for_users.py
@@ -48,33 +48,32 @@ query='select user_id,last(date) from week3_try1 group by  user_id'
 result = client.query(query)
 res=result.raw
 count =0
-serie=[]
 series=res['series']
-serie.append(series)
+print series
 start_time=time.time()
-for ser in serie:
-	count +=1
-	strg=ser['values'][0]
-	que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1 where user_id='"+strg[1]+"' and date='"+strg[2]+"' group by user_id"
-	res = client.query(que)
-	res1= res.raw
-	series=res1['series'][0]
-	vals=series['values'][0]
-	tags=series['tags']
-	user_id=tags['user_id']
-	speed=vals[1]
-	calories_rate=vals[1]
-	heart_rate=vals[2]
+# for ser in series:
+# 	count +=1
+# 	strg=ser['values'][0]
+# 	que = "select mean(speed),mean(calories_rate),mean(heart_rate) from week3_try1 where user_id='"+strg[1]+"' and date='"+strg[2]+"' group by user_id"
+# 	res = client.query(que)
+# 	res1= res.raw
+# 	series=res1['series'][0]
+# 	vals=series['values'][0]
+# 	tags=series['tags']
+# 	user_id=tags['user_id']
+# 	speed=vals[1]
+# 	calories_rate=vals[1]
+# 	heart_rate=vals[2]
 
-	bmi=random.randrange(18,35)
-	fat=random.randrange(15,25)
-	steps=random.randrange(1000,10000)
-	floors=random.randrange(0,25)
-	calories=random.randrange(1500,3000)
-	data2=[{"measurement":"week4_final1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}]
-	client.write_points(data2)
+# 	bmi=random.randrange(18,35)
+# 	fat=random.randrange(15,25)
+# 	steps=random.randrange(1000,10000)
+# 	floors=random.randrange(0,25)
+# 	calories=random.randrange(1500,3000)
+# 	data2=[{"measurement":"week4_final1","tags":{"user_id":user_id},"fields":{"bmi":int(bmi),"fat":int(fat),"steps":int(steps),"floors":int(floors), "calories":int(calories), "speed":int(float(speed)), "calories_rate":int(float(calories_rate)),"heart_rate":int(float(heart_rate))}}]
+# 	client.write_points(data2)
 
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
