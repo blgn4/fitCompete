c168c2f6d0e431684b12b050ca0b79e359a60699
blgn4 <blgniharika@hotmail.com>
2016-06-26 12:35:24 -0700

    flask streaming inclusion

--- FLask/app/static/js/competetion.js
+++ FLask/app/static/js/competetion.js
@@ -1,4 +1,9 @@
 $(function() {
+
+   $('.user_list ').on('click' , function(e) {
+        console.log(e)
+   });
+
     $('a#gen_stream').bind('click', start_stream)
     
     
@@ -17,24 +22,42 @@ function get_data(){
           //console.log(str1)
           var str2 = data.result[1][1]
           //console.log(str2)
+          def_data=[0,0,0]
           var dat1 = str1.split(',')
           var dat2 = str2.split(',')
-          if (dat1[0] == 1 ) usr1 = dat1; 
-          else if (dat2[0] == 1 ) usr1=dat2;
-          else usr1=[0,0];
+          if (dat1[0] == 1 ) render_table1(data1)
+          else if (dat2[0] == 1 ) render_table1(data2)
+          else render_table1(def_data)
 
-          if (dat2[0] == 0 ) usr2 = dat2; 
-          else if (dat1[0] == 0 ) usr2=dat1;
-          else usr2=[0,0];
+          if (dat2[0] == 0 ) render_table2(data2)
+          else if (dat1[0] == 0 ) render_table2(data1)
+          else render_table2(def_data)
           console.log(usr1)
           console.log(usr2)
-          dat=[{"letter":usr1[0], "frequency":Number(usr1[1])},{"letter":usr2[0], "frequency":Number(usr2[1])}]
-          render_graph(dat)
+          
 });
         
 };
 
+function render_table1(data)
+{
+  var table1 =  "<table><tbody>";
+  table1 += '<tr>  <th>User_ID:</th> <td> User: '+ $('#user_id').val() +' </td> </tr>'
+  table1 += '<tr> <th> Average Run rate per 3 secs </th> <td> '+ data[1] +'</td> </tr>'
+  table1 += '<tr> <th> Pace </th> <td> '+ data[2] +'</td> </tr>'
+  table1+='</tbody> </table>'
+    $('.box1').html(table1)
+}
 
+function render_table2(data)
+{
+     var table2 =  "<table><tbody>";
+  table2 += '<tr>  <th>User_ID:</th> <td> User: '+ $('#user_id').val() +' </td> </tr>'
+  table2 += '<tr> <th> Average Run rate per 3 secs </th> <td> '+ data[1] +'</td> </tr>'
+  table2 += '<tr> <th> Pace </th> <td> '+ data[2] +'</td> </tr>'
+  table2+='</tbody> </table>'
+    $('.box1').html(table2)
+}
 
 
 function render_graph(data) {
