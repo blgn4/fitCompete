f7eac3d2fecb1cdfb0010d6b798a7c5634dca2ab
blgn4 <blgniharika@hotmail.com>
2016-06-26 19:10:17 -0700

    added influx consumer

---
+++ kafka/Influx_Consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+from influxdb import InfluxDBClient
+
+consumer = KafkaConsumer('user_timeseries',bootstrap_servers=["52.41.140.111:9092","52.41.90.5:9092","52.41.120.152:9092"])
+
+for msg in consumer:
+        data=msg.value
+        print data
+        d=data.split(",")
+        data1=[{"measurement":"fit_timeseries","tags":{"user_id":int(d[1])},"time":int(d[0]),"fields":{"heart_rate":int(d[1]),"speed":int(d[2]),"calories_rate":int(d[3]),"date":d[4]}}]
+        client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
+        client.write_points(data1)
+
+
+
