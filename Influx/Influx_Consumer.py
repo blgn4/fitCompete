955f0a74b629c7b0476882e3b041df2faca68b0b
blgn4 <blgniharika@hotmail.com>
2016-06-26 19:19:47 -0700

    changes made to Influx Consumer

--- Influx/Influx_Consumer.py
+++ Influx/Influx_Consumer.py
@@ -1,13 +1,13 @@
 from kafka import KafkaConsumer
 from influxdb import InfluxDBClient
 
-consumer = KafkaConsumer('user_timeseries',bootstrap_servers='localhost:9092')
+consumer = KafkaConsumer('user_timeseries',bootstrap_servers=["52.41.140.111:9092","52.41.90.5:9092","52.41.120.152:9092"])
 
 for msg in consumer:
         data=msg.value
         print data
         d=data.split(",")
-        data1=[{"measurement":"fit_timeseries","tags":{"user_id":int(d[1])},"time":int(d[0]),"fields":{"heart_rate":int(d[1]),"speed":int(d[2]),"calories_rate":int(d[3]),"date":d[4]}}]
+        data1=[{"measurement":"fit_timeseries","tags":{"user_id":int(d[1])},"time":d[0],"fields":{"heart_rate":int(d[2]),"speed":int(d[3]),"calories_rate":int(d[4]),"date":d[5]}}]
         client=InfluxDBClient('ec2-52-10-176-111.us-west-2.compute.amazonaws.com',8086,'root','root','niha')
         client.write_points(data1)
 
